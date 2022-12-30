d840d422b8a5b9739495d456dbfc901c4858ea33
Andu <andualemalemayehuwku@gmail.com>
2022-12-30 13:07:26 +0300

    done!

--- myWebServer.js
+++ myWebServer.js
@@ -65,16 +65,119 @@ const PORT = 1234;
 //   console.log(`My express server listening at http://localhost:${PORT}`);
 // });
 
-//--express static
+// //--express static
+
+// const express = require("express");
+// const serverApp = express();
+// serverApp.use("/", express.static("static"));
+// serverApp.listen(PORT, function (err) {
+//   if (err) {
+//     console.log(err);
+//   }
+
+//   console.log(`My express listening at http://localhost:${PORT}`);
+
+// });
+
+//week 3 MySQL practice
+//###--db connection
+//--question 1
 
 const express = require("express");
+const mysql = require("mysql");
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "myDB",
+  password: "myDB",
+  database: "mydb",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+// question 2 create five tables
+
 const serverApp = express();
-serverApp.use("/", express.static("static"));
+// serverApp.get("/install", function (req, res) {
+//   //create product table
+//   let Products = `CREATE TABLE if not exists Products(
+//       product_id int auto_increment,
+//       product_url varchar(255) not null,
+//       product_name varchar(255) not null,
+//       PRIMARY KEY (product_id)
+//   )`;
+//   con.query(Products, (err, results) => {
+//     if (err) console.log(err);
+//     console.log("<h1>created product table<h1/>");
+//   });
+
+//   // create ProductDescription table
+//   let ProductDescription = `CREATE TABLE if not exists ProductDescription(
+//     description_id int auto_increment,
+//     product_id int(11) not null,
+//     product_brief_description TEXT not null,
+//     product_description TEXT not null,
+//     product_img varchar(255) not null,
+//     product_link varchar(255) not null,
+//     PRIMARY KEY (description_id),
+//     FOREIGN KEY (product_id) REFERENCES Products(product_id)
+//   )`;
+
+//   con.query(ProductDescription, (err, results) => {
+//     if (err) console.log(err);
+//     console.log("<h1>created ProductDescription table<h1/>");
+//   });
+
+//   // create table ProductPrice
+//   let ProductPrice = `CREATE TABLE if not exists ProductPrice(
+//     price_id int auto_increment,
+//     product_id int(11) not null,
+//     starting_price varchar(255) not null,
+//     price_range varchar(255) not null,
+//     PRIMARY KEY (price_id),
+//     FOREIGN KEY (product_id) REFERENCES Products(product_id)
+//   )`;
+//   con.query(ProductPrice, (err, results) => {
+//     if (err) console.log(err);
+//     console.log("<h1> created ProductPrice table<h1/>");
+//   });
+
+//   // create table user
+//   let user = `CREATE TABLE if not exists User(
+//     user_id int auto_increment,
+//     user_name varchar(255) not null,
+//     user_password varchar(255) not null,
+//     PRIMARY KEY (user_id)
+//   )`;
+//   con.query(user, (err, results) => {
+//     if (err) console.log(err);
+//     console.log("<h1>created user table<h1/>");
+//   });
+
+//   //create order table
+
+//   let order = `CREATE TABLE if not exists Orders(
+//     order_id int auto_increment,
+//     product_id int(11) not null,
+//     user_id int(11) not null,
+//     PRIMARY KEY (order_id),
+//     FOREIGN KEY (product_id) REFERENCES Products(product_id),
+//     FOREIGN KEY (user_id) REFERENCES User(user_id)
+//   )`;
+//   con.query(order, (err, results) => {
+//     if (err) console.log(err);
+//     res.send("<h1>created table<h1/>");
+//   });
+// });
+
+serverApp.use("/form", express.static("display"));
+
 serverApp.listen(PORT, function (err) {
   if (err) {
     console.log(err);
   }
- 
-  console.log(`My express listening at http://localhost:${PORT}`);
-
+  console.log(`My express server listening at http://localhost:${PORT}`);
 });
